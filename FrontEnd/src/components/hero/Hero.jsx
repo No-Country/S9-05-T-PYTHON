100930dfddb88792f01006afacfa807ce4dede16
courfedra <franandres93@gmail.com>
2023-07-04 16:39:25 -0300

    Re design home

--- FrontEnd/src/components/hero/Hero.jsx
+++ FrontEnd/src/components/hero/Hero.jsx
@@ -1,7 +1,14 @@
 const Hero = () => {
     return (
-        <section className="">
-            <img className="w-full object-cover" src="/hero.webp" alt="hero" />
+        <section className="bg-gradient-to-tl from-red-200 to-violet-300 flex justify-evenly items-center w-screen h-screen">
+            <div className="w-[50%] flex flex-col justify-center items-center">
+                <p>Matchea ahora!</p>
+                <p>El sitio web perfecto para encontrar el alma gemela de tu mascota</p>
+                <button className="border p-2 rounded-xl hover:bg-violet-400">Buscar!</button>
+            </div>
+            <div className="w-[50%] ">
+                <img src="/hero.webp" alt="dogHero"/>
+            </div>
         </section>
     )
 }
