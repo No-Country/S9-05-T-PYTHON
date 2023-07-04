8ac7f184b38bd3089b12f656a0c7780b03e3bca8
vic-ferr <victorhfp2014@gmail.com>
2023-07-04 01:08:25 -0400

    home

---
+++ FrontEnd/src/components/hero/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return (
+        <section className="">
+            <img className="w-full object-cover" src="/hero.webp" alt="hero" />
+        </section>
+    )
+}
+
+export default Hero
