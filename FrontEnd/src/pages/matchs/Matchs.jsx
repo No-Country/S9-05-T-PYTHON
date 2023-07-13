a21fc7cc3660d07ab28f05e9132c01c6cd620ba1
courfedra <franandres93@gmail.com>
2023-07-13 19:25:02 -0300

    fix scroll-top when open matchs

--- FrontEnd/src/pages/matchs/Matchs.jsx
+++ FrontEnd/src/pages/matchs/Matchs.jsx
@@ -1,6 +1,7 @@
 import CardMatch from "./components/CardMatch"
 import dataCardSlider from "../../utils/dataCardSlider.json";
 const Matchs=()=>{
+    window.scrollTo(0, 0);
     return(
         <div className="w-[100%] min-h-[100vh] flex flex-col justify-center items-center">
             <div className="w-[80%] min-h-[100vh] pt-[10em] flex flex-wrap justify-center items-center ">
