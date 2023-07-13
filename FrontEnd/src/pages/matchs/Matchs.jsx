0ac1be3785ee10e69af0440fe921789053c711b5
courfedra <franandres93@gmail.com>
2023-07-13 09:15:38 -0300

    create Matchs component

---
+++ FrontEnd/src/pages/matchs/Matchs.jsx
@@ -0,0 +1,14 @@
+import CardMatch from "./components/CardMatch"
+import dataCardSlider from "../../utils/dataCardSlider.json";
+const Matchs=()=>{
+    return(
+        <div className="w-[100%] min-h-[100vh] flex flex-col justify-center items-center">
+            <div className="w-[80%] min-h-[100vh] pt-[10em] flex flex-wrap justify-center items-center ">
+                {dataCardSlider.map((info)=>(
+                    <CardMatch info={info} key={info.id}/>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default Matchs
